6f9a9bc8ca9dd7580778fcb4debb6d5b0c821413
Kai <kai@tocaboca.com>
2016-06-09 00:17:40 +0200

    Make class static

--- sample/src/main/java/com/hudomju/swipe/sample/RecyclerViewActivity.java
+++ sample/src/main/java/com/hudomju/swipe/sample/RecyclerViewActivity.java
@@ -81,7 +81,9 @@ public class RecyclerViewActivity extends Activity {
                 }));
     }
 
-    class MyBaseAdapter extends RecyclerView.Adapter<MyBaseAdapter.MyViewHolder> {
+    //keep class static, because:
+    //当你在Activity中使用内部类的时候，需要时刻考虑您是否可以控制该内部类的生命周期，如果不可以，则最好定义为静态内部类
+    static class MyBaseAdapter extends RecyclerView.Adapter<MyBaseAdapter.MyViewHolder> {
 
         private static final int SIZE = 100;
 
@@ -115,7 +117,9 @@ public class RecyclerViewActivity extends Activity {
             notifyItemRemoved(position);
         }
 
-        class MyViewHolder extends RecyclerView.ViewHolder {
+        //keep class static, because:
+        //当你在Activity中使用内部类的时候，需要时刻考虑您是否可以控制该内部类的生命周期，如果不可以，则最好定义为静态内部类
+        static class MyViewHolder extends RecyclerView.ViewHolder {
 
             TextView dataTextView;
             MyViewHolder(View view) {
